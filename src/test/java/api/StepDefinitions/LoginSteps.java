9f35706d09b0bd58cba093ac3059fe25afd4f671
neethzp <neethu.perunthody@gmail.com>
2024-11-21 18:27:11 -0500

    work

--- src/test/java/api/StepDefinitions/LoginSteps.java
+++ src/test/java/api/StepDefinitions/LoginSteps.java
@@ -6,48 +6,49 @@ import java.util.List;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+
 import java.util.Map;
 
+import org.testng.Assert;
+
+import java.io.FileWriter;
 import api.Utility.ExcelReader;
+import static io.restassured.RestAssured.given;
+import static org.testng.Assert.assertEquals;
+
+
 
 public class LoginSteps {
 	
 	private List<Map<String, String>> excelData;
 	private Map<String, String> currentRow;
 	
+    private RequestSpecification requestSpec;
+    private Response response;
+   
 	@Given("Admin creates request with required credentials for scenario")
 	public void admin_creates_request_with_required_credentials_for_scenario() throws IOException {
-		if (excelData == null) {
-            String filePath = "C:/path/to/Team3-API Warriors Test Data.xlsx";
-            excelData = ExcelReader.readExcelData(filePath, "LoginTests");
-        }
+		
+    }
 
-        // Iterate through Excel rows
-        for (Map<String, String> row : excelData) {
-            currentRow = row;
-            String email = currentRow.get("EmailId");
-            String password = currentRow.get("Password");
-
-//            // Build request body
-//            LoginRequest loginRequest = new LoginRequest(email, password);
-//
-//            // Build request specification
-//            requestSpec = given()
-//                    .contentType("application/json")
-//                    .body(loginRequest);
-        }
-	}
+        
+	
 
 	@When("Admin calls API")
 	public void admin_calls_api() {
-	   
+		
 		 
 	}
 
 	@Then("Admin validates the response")
 	public void admin_validates_the_response() {
-	    
-	}
+	
+        }
+    
+
+    
 
 
 }
--- src/test/java/api/Utility/ExcelReader.java
+++ src/test/java/api/Utility/ExcelReader.java
@@ -9,7 +9,7 @@ import java.util.*;
 
 public class ExcelReader {
 
-    public static List<Map<String, String>> readExcelData(String filePath, String sheetName) throws IOException {
+    /*public static List<Map<String, String>> readExcelData(String filePath, String sheetName) throws IOException {
         FileInputStream fis = new FileInputStream(filePath);
         Workbook workbook = new XSSFWorkbook(fis);
         Sheet sheet = workbook.getSheet(sheetName);
@@ -33,6 +33,34 @@ public class ExcelReader {
                 }
             }
 
+            data.add(rowData);
+        }
+
+        workbook.close();
+        fis.close();
+        return data;
+    }*/
+    public static List<Map<String, String>> readExcelData(String filePath, String sheetName) throws IOException {
+        List<Map<String, String>> data = new ArrayList<>();
+        FileInputStream fis = new FileInputStream(filePath);
+        Workbook workbook = new XSSFWorkbook(fis);
+        Sheet sheet = workbook.getSheet(sheetName);
+
+        // Read headers
+        Row headerRow = sheet.getRow(0);
+        List<String> headers = new ArrayList<>();
+        for (Cell cell : headerRow) {
+            headers.add(cell.getStringCellValue());
+        }
+
+        // Read data rows
+        for (int i = 1; i <= sheet.getLastRowNum(); i++) {
+            Row dataRow = sheet.getRow(i);
+            Map<String, String> rowData = new HashMap<>();
+            for (int j = 0; j < headers.size(); j++) {
+                Cell cell = dataRow.getCell(j);
+                rowData.put(headers.get(j), cell.toString());
+            }
             data.add(rowData);
         }
 
