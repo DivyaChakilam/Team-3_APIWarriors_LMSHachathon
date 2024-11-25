919e3545107563679e04286510bcef1c6f77fe4f
DivyaChakilam <161245630+DivyaChakilam@users.noreply.github.com>
2024-11-24 21:58:05 -0500

    optmized code with post program and put program

--- src/test/java/api/payload/LoginPayload.java
+++ src/test/java/api/payload/LoginPayload.java
@@ -3,29 +3,27 @@ package api.payload;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import api.Utility.CommonUtils;
-import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Data // Generates getters, setters, toString, equals, and hashCode methods
 //@NoArgsConstructor // Generates a no-arguments constructor
 //@AllArgsConstructor // Generates an all-arguments constructor
 public class LoginPayload extends CommonUtils {
 
-	 @JsonProperty("userLoginEmailId")
+	@JsonProperty("userLoginEmailId")
 	private String email;
-	 @JsonProperty("password")
-	  private String password;
-	  public LoginPayload() {}
-	  public LoginPayload(String email, String password) {
-	        this.email = email;
-	        this.password = password;
-	    }
+	@JsonProperty("password")
+	private String password;
+	public LoginPayload() {}
+	public LoginPayload(String email, String password) {
+		this.email = email;
+		this.password = password;
+	}
 	// Getters and Setters
-	    public String getPassword() {
-	        return password;
-	    }
-	    public void setPassword(String password) {
-	        this.password = password;
-	    }
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
 }
